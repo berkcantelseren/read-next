0446f22324efe3f2d73726cc28c6a601efdfb3a0
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-05 22:18:56 +0300

    changed navbar h - w

--- app/components/Home/Navbar/Navbar.tsx
+++ app/components/Home/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 export default function Navbar() {
   return (
-    <div className="h-[8vh]">
-      <div>
+    <div className="h-[8vh] bg-amber-200">
+      <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+        {/* {ReadNext Logo} */}
         <div>Test</div>
       </div>
     </div>
