4d0f35dbcd62c85da56bae81e2d4eaec1b9cf0cd
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-05 23:05:30 +0300

    fixed hero section

--- app/components/Home/Hero/Hero.tsx
+++ app/components/Home/Hero/Hero.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="bg-[#212121] text-white pt-[12vh] pb-24">
-      <div className="container w-[90%] xl:w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-12">
+    <section className="pt-[12vh] pb-24">
+      <div className="container w-[90%] xl:w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-40">
         {/* Left Section */}
         <div>
-          <h1 className="text-5xl font-bold text-orange-500 leading-tight">
+          <h1 className="text-5xl font-bold text-[#E19B05]">
             Discover Your Next Favorite Book
           </h1>
-          <p className="mt-4 text-lg text-gray-300">
-            Get personalized book recommendations tailored just for you.
+          <p className="mt-10 text-lg text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum,
+            ad labore unde dignissimos quidem.
           </p>
-          <button className="mt-6 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition">
+          <button className="mt-10 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition">
             Get Started
           </button>
         </div>
