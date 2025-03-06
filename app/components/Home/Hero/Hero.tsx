3271f46e52750145f36567cb87eb6c5f458d122b
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-06 21:05:42 +0300

    hero section right grid changed and tailwind config changes

--- app/components/Home/Hero/Hero.tsx
+++ app/components/Home/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import { Saira } from "next/font/google";
+import { FaArrowDown, FaBook } from "react-icons/fa";
 
 const DancingFont = Saira({
   weight: "400",
@@ -29,13 +30,42 @@ export default function Hero() {
         </div>
 
         {/* Right Section */}
-        <div className="flex justify-center">
-          <Image
-            src="/images/books.png"
-            alt="book images"
-            height={1000}
-            width={1000}
-          />
+        <div className="flex flex-col items-center space-y-6">
+          {/* First Box */}
+          <div
+            className={`${DancingFont.className} border border-gray-300 px-6 py-3 text-white w-60 h-[73.6px] flex items-center justify-center text-center rounded-lg`}
+          >
+            Type the book you love
+          </div>
+
+          {/* Downward Arrow */}
+          <FaArrowDown className="text-white" />
+          {/* Second Box */}
+          <div
+            className={`${DancingFont.className} border border-gray-300 px-6 py-3 text-white w-60 text-center rounded-lg`}
+          >
+            We analyze similar books for you
+          </div>
+
+          {/* Downward Arrow */}
+          <FaArrowDown className="text-white" />
+
+          {/* Third Box */}
+          <div className="flex -space-x-4">
+            <div
+              className={`${DancingFont.className} border border-gray-300 px-6 py-3 text-white w-60 text-center rounded-lg mb-10`}
+            >
+              Find best books we think you'll love
+            </div>
+          </div>
+          <div className="flex space-x-3">
+            <FaBook className="w-8 h-8 text-[#F28D8D] animate-rotate" />
+            <FaBook className="w-8 h-8 text-[#A0C4FF] animate-rotate" />
+            <FaBook className="w-8 h-8 text-[#D4A6F7] animate-rotate" />
+            <FaBook className="w-8 h-8 text-[#F9E4A1] animate-rotate" />
+            <FaBook className="w-8 h-8 text-[#A4E1A1] animate-rotate" />
+            <FaBook className="w-8 h-8 text-[#FFABAB] animate-rotate" />
+          </div>
         </div>
       </div>
       <hr className="w-[90%] xl:w-[80%] mx-auto border-t border-gray-600 my-12" />
